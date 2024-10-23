9387def297ae628e036af447deb9a1baada1b26d
AbderrafieElmrabet <abderafieelmrabet@gmail.com>
2024-10-23 09:33:30 +0100

    tuto 1.2

---
+++ resources/views/Layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon application Laravel</title>
+</head>
+
+<body>
+    <header>
+        <h1>Mon site</h1>
+        <nav>
+        </nav>
+    </header>
+    @yield('content')
+    <footer>
+        &copy; 2023 Tous droits réservés.
+    </footer>
+</body>
+
+</html>
