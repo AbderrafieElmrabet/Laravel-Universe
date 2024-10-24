61d93ac96ee90ee133bb36c86588b561a3bc12fa
AbderrafieElmrabet <abderafieelmrabet@gmail.com>
2024-10-24 09:15:52 +0100

    Analyse

--- Tutorials/app/Http/Controllers/contactController.php
+++ Tutorials/app/Http/Controllers/contactController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Mail;
 
-class contactController extends Controller
+class ContactController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('contact');
+    }
+    // public function store(Request $request)
+    // {
+    //     $data = $request->validate([
+    //         'name' => 'required',
+    //         'email' => 'required|email',
+    //         'message' => 'required'
+    //     ]);
+    //     Mail::send('email.contact', $data, function ($message) use ($data) {
+    //         $message->to('abderafieelmrabet@gmail.com', 'Abderrafie Laravel')->subject('New contact Message');
+    //     });
+    //     //
+    //     return redirect('/')->with('success', 'your message was sent.');
+    // }
 }
--- Tutorials/routes/web.php
+++ Tutorials/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (): Factory|View {
+    return view('contact');
 });
 Route::get('/about', function () {
     return view('about');
 });
+Route::get('/contact', [ContactController::class, 'create']);
+// Route::post('/contact', [ContactController::class, 'store']);
