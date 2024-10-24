dbc5a7e8fdb19e15aeb19a5d463d44256224f035
AbderrafieElmrabet <abderafieelmrabet@gmail.com>
2024-10-24 16:47:52 +0100

    prototype

--- Prototype/resources/views/Layouts/app.blade.php
+++ Prototype/resources/views/Layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Laravel Application</title>
+    <title>Prototype</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
