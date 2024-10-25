e5faf44ced6ddbc0c3d3e1c3d70b53ed7d7dd749
AbderrafieElmrabet <abderafieelmrabet@gmail.com>
2024-10-25 10:39:38 +0100

    Conception

--- Prototype/app/Http/Controllers/contactController.php
+++ Prototype/app/Http/Controllers/contactController.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Mail;
 
-class ContactController extends Controller
+class contactController extends Controller
 {
     public function create()
     {
